2fc12526ff360559cb71a6e9076674eb7fa4aff5
lui <ladccc@cesar.school>
2024-06-26 05:42:00 -0300

    fix: change password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,6 +19,28 @@ const login = (req, res) => {
     });
 };
 
+// Função para troca de senha
+const changePassword = (req, res) => {
+    const { email, senhaAtual, novaSenha } = req.body;
+    pool.query('SELECT * FROM cliente WHERE email = ? AND senha = ?', [email, senhaAtual], (error, results) => {
+        if (error) {
+            return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+        }
+        if (results.length > 0) {
+            const cliente = results[0];
+            pool.query('UPDATE cliente SET senha = ? WHERE id = ?', [novaSenha, cliente.id], (updateError, updateResults) => {
+                if (updateError) {
+                    return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+                }
+                return res.status(200).json({ status: 'success', message: 'Password changed successfully' });
+            });
+        } else {
+            return res.status(401).json({ status: 'error', message: 'Current password is incorrect or user not found' });
+        }
+    });
+};
+
 module.exports = {
-    login
+    login,
+    changePassword
 };
