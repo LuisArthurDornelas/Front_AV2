de0a2f632ea54b5d238bd053e0234a41daf714e6
lui <ladccc@cesar.school>
2024-06-26 01:54:42 -0300

    fix auth route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.post('/auth', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
